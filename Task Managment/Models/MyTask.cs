20df81bc0529a97dc834cbf8f7094b0864eec532
lehquy13 <lehquy13@gmail.com>
2022-04-07 15:38:39 +0700

    re-organise a littile bit

---
+++ Task Managment/Models/MyTask.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class MyTask
+    {
+        [Key] public int TaskId { get; set; }
+        public int ListId { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        //navigation property
+        public MyList List { get; set; }
+    }
+}
