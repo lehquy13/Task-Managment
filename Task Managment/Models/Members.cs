c318e2b14f848692c6627994692d324458af8ff1
phatlam1811@gmail.com <phatlam1811@gmail.com>
2022-04-13 00:00:26 +0700

    Add model Members

---
+++ Task Managment/Models/Members.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Task_Managment.Models
+{
+    internal class Members
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Members(string email, string username, string password)
+        {
+            Email = email;
+            UserName = username;
+            Password = password;
+        }
+
+        public Members(Members refObj)
+        {
+            Email = refObj.Email;
+            UserName = refObj.UserName;
+            Password = refObj.Password;
+        }
+    }
+}
