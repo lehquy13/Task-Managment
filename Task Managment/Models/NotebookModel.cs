37c55dea6c6a49503ae1ca474dd2990e50dd7c85
phatlam1811@gmail.com <phatlam1811@gmail.com>
2022-05-04 18:11:47 +0700

    Add note & notebooks models

---
+++ Task Managment/Models/NotebookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Task_Managment.Models
+{
+    public class NotebookModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public string _ownerId { get; set; }
+        public string _name { get; set; }
+        public ObservableCollection<Note> _collection { get; set; }
+        public DateTime _createdDate { get; set; }
+    }
+}
