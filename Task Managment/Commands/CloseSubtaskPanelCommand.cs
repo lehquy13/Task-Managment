ee9423e5426eed53006af8ff30648379e6cad55d
lehquy13 <lehquy13@gmail.com>
2022-05-04 18:37:30 +0700

    fix hide and unhide subtask details

---
+++ Task Managment/Commands/CloseSubtaskPanelCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task_Managment.ViewModels;
+
+namespace Task_Managment.Commands
+{
+    public class CloseSubtaskPanelCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        TasksViewModel TasksViewModels { get; set; }
+
+
+        //!Ctor
+        public CloseSubtaskPanelCommand(TasksViewModel tasksViewModel)
+        {
+            this.TasksViewModels = tasksViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            TasksViewModels.SubtasksPaneVisible = false;
+        }
+    }
+}
---
+++ Task Managment/Commands/SelectSubtaskCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task_Managment.Models;
+using Task_Managment.ViewModels;
+using Task = Task_Managment.Models.Task;
+
+namespace Task_Managment.Commands
+{
+    public class SelectSubtaskCommand : ICommand
+    {
+
+        //!Fields
+
+        //!Properties
+        public TasksViewModel TasksViewModel { get; set; }
+
+        //!Events
+        public event EventHandler CanExecuteChanged;
+
+        //!Ctor
+        public SelectSubtaskCommand(TasksViewModel tasksViewModel)
+        {
+            this.TasksViewModel = tasksViewModel;
+        }
+
+        //!Methods
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //parameter is the TasksViewModel object
+            if (parameter is Task)
+            {
+                Task task = (Task)parameter;
+                TasksViewModel.SelectedTask = task;
+            }
+
+            return;
+
+            ////create a new string subtask
+            //Subtask newSubtask = new Subtask(this.TasksViewModel.SelectedTask.TaskID)
+            //{
+            //    Name = ""
+            //};
+
+            ////add the string to the observable collection
+            //this.TasksViewModel.Subtasks.Add(newSubtask);
+
+            ////add the string to the subtasks list
+            //this.TasksViewModel.SelectedTask.Subtasks.Add(newSubtask);
+        }
+    }
+}
