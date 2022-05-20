a4f051f5031696016ae951627fbabe594d7376c8
lehquy13 <lehquy13@gmail.com>
2022-05-20 21:31:02 +0700

    focus bug fixed

--- Task Managment/Commands/TaskCommands/DeleteCommand.cs
+++ Task Managment/Commands/TaskCommands/DeleteCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Task_Managment.Models;
 using Task_Managment.ViewModels;
@@ -38,8 +39,14 @@ namespace Task_Managment.ViewModel.Commands
 
         public void Execute(object parameter)
         {
-            if(parameter is Task)
+            if(parameter is ListViewItem)
             {
+                ListViewItem item = (ListViewItem)parameter;
+                this.TasksViewModels.SelectedTask = item.Content as Task;
+            }
+            if (parameter is Task || parameter is ListViewItem)
+            {
+                this.TasksViewModels.SubtasksPaneVisible = !this.TasksViewModels.SubtasksPaneVisible;
                 Task selectedTask = this.TasksViewModels.SelectedTask;
                 //remove it from the observable collection if its not null
                 this.TasksViewModels.TasksList?.Remove(selectedTask);
@@ -48,8 +55,9 @@ namespace Task_Managment.ViewModel.Commands
                 db.DeleteSelectedTask(selectedTask);
                 //update the Totalcount of this.TasksViewModels.SelectedTasklist
                 this.TasksViewModels.SelectedTasklist.TotalCount =
-                        this.TasksViewModels.SelectedTasklist.Tasks.Count.ToString();
+                this.TasksViewModels.SelectedTasklist.Tasks.Count.ToString();
                 db.UpdateSelectedTasklist(this.TasksViewModels.SelectedTasklist);
+                this.TasksViewModels.SubtasksPaneVisible = (this.TasksViewModels.SubtasksPaneVisible == true) ? false : true;
             }
 
             else if(parameter is Tasklist)
